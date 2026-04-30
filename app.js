cdcbcd144c357991bef868e80110f31db4f24cc4
dharamjeet875-droid <dharamjeet875@gmail.com>
2026-04-30 15:59:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
+alert("app.js loaded");
 const SUPABASE_URL = "https://jpaotjnlsetarbejlqry.supabase.co/rest/v1/";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwYW90am5sc2V0YXJiZWpscXJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MzQ5MzIsImV4cCI6MjA5MzExMDkzMn0.u3gKFA2Fy8W4eJIk0fu3zlkO2-Ujvhzi-8zxCeHnR3I";
